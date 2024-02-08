bd9ceac2afba8dd92ad4092e5ea92db9a23567ca
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-08 12:19:23 -0800

    Begin to Add States - WIP

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,21 @@
 class Player extends Phaser.Physics.Arcade.Sprite  {
-    
+    /**
+     * Represents a player object.
+     * @constructor
+     * @param {Phaser.Scene} scene - The scene in which the player exists.
+     * @param {number} x - The x-coordinate of the player's initial position.
+     * @param {number} y - The y-coordinate of the player's initial position.
+     * @param {string} texture - The texture key for the player's sprite.
+     */
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture)
-        this.stateMachine = new StateMachine(new IdleState(this))
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+        this.stateMachine = new StateMachine(0, {
+            idle: new IdleState(this.stateMachine),
+            run: new RunState(this.stateMachine),
+            jump: new JumpState(this.stateMachine),
+        }, [this])
     }
 }
 
@@ -16,11 +29,12 @@ class IdleState extends State {
         }
     }
 
-    enter() {
+    enter(player) {
         this.stateMachine.sprite.anims.play('idle')
     }
 }
-class RunningState extends State {
+
+class RunState extends State {
     
     constructor(stateMachine) {
         super(stateMachine)
@@ -29,11 +43,24 @@ class RunningState extends State {
         }
     }
 
-    enter() {
+    enter(player) {
         this.stateMachine.sprite.anims.play('run')
     }
 
-    update() {
+    update(player) {
         // Check for input and change state
+    }
+}
+
+class JumpState extends State {
+    constructor(stateMachine) {
+        super(stateMachine)
+        this.transitions = {
+
+        }
+    }
+
+    enter(player) {
+
     }
 }
