148f9bcce1b0dea5af3342d44a97117b4ab3ce4d
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-12 15:41:56 -0800

    Use Matter physics and setup global control scheme

--- src/main.js
+++ src/main.js
@@ -6,11 +6,17 @@ const config = {
     height: 720,
     scene: [MainMenu, Play, GameOverMenu, CreditsMenu],
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {x:0, y: 0},
-            debug: true,
-        },
+        default: 'matter',
+        matter: {
+            enableSleeping: true,
+            gravity: {
+                y: 0
+            },
+            debug: {
+                showBody: true,
+                showStaticBody: true
+            }
+        }
     },
     render: {
         pixelArt: true,
@@ -21,5 +27,14 @@ const game = new Phaser.Game(config)
 
 const gameSettings = {
     width: config.width,
-    height: config.height
+    height: config.height,
+    cameraFollowStrength: {x: .8, y: .8},
+    controls: {
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        jump: Phaser.Input.Keyboard.KeyCodes.W,
+        grapple: Phaser.Input.Keyboard.KeyCodes.SPACE,
+    },
+    spawnLocation: {x: 500, y: 500},
 }
