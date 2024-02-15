a75e8fdee0a97a6996134ba0281eb72d5f4bfbfd
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-14 17:53:13 -0800

    Add SoundEffect class and refactored SFXManager to inherit Scene so that it can play sounds as a persistent, invisble scene.

--- src/SFXManager.js
+++ src/SFXManager.js
@@ -1,16 +1,41 @@
 "use strict"
 
-class SFXManager {
+class SFXManager extends Phaser.Scene {
     static Instance = null;
     #sfx = {};
     
-    constructor(sfx) {
+    constructor(sfx, soundManagerPlugin) {
         if (this.Instance != null) {
             return null
         } else {
-            SFXManager.Instance = this
+            super({key: 'SFXManager', visible: false, active: true});
+            this.sound = soundManagerPlugin;
+            SFXManager.Instance = this;
         }
-        this.#sfx = sfx
+        this.#sfx = sfx;
         this.currentlyPlaying = {};
+    }
+
+    /**
+     * Plays the specified sound effect if it is loaded and can play.
+     * @param {string} soundEffectName 
+     * @returns true if sound effect played else false
+     */
+    play(soundEffectName) {
+        if (soundEffectName in this.#sfx) {
+            let sfx = this.#sfx[soundEffectName];
+            if (sfx.canPlay()) {
+                sfx.play();
+                return true;
+            }
+            else {
+                console.log(`Max number of allowed ${soundEffectName} already playing`)
+                return false;
+            }
+        }
+        else {
+            console.log(`${soundEffectName} is not a valid sound effect`)
+            return false;
+        }
     }
 }
