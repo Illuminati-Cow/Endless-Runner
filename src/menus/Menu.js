7fe68fd2e8eeadd3d4cff9f2f062adc0faadc9cb
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-14 16:47:07 -0800

    Refactor Menu class to use scene manager plugin which fixes null reference to this.scene

--- src/menus/Menu.js
+++ src/menus/Menu.js
@@ -1,15 +1,22 @@
 "use strict"
 
 class Menu extends Phaser.Scene {
+    /**
+     * The scene manager for the game.
+     * @type{Phaser.Scenes.ScenePlugin}
+     */
+    static sceneManagerPlugin
     constructor(sceneName) {
-        super(sceneName)
+        super({key: sceneName, visible: false, active: false})
     }
 
     openMenu() {
-        this.scene.isActive(true);
+        Menu.sceneManagerPlugin.setActive(true, this);
+        Menu.sceneManagerPlugin.setVisible(true, this);
     }
 
     closeMenu() {
-
+        Menu.sceneManagerPlugin.setVisible(false, this);
+        Menu.sceneManagerPlugin.setActive(false, this);
     }
 }
