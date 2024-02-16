9e846400b75e58fd9642fec219a73610d9f78ae3
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-15 23:38:39 -0800

    Added music credit

--- src/menus/CreditsMenu.js
+++ src/menus/CreditsMenu.js
@@ -13,7 +13,7 @@ class CreditsMenu extends Menu {
         let button = new Button(this, 0, 0, width/10, width/10, "X",
             () => {UIManager.Instance.changeMenu('MainMenu')}, undefined, 'topleft');
         this.add.text(width/2, height/2, 
-            "Programming: Cole Falxa-Sturken\nArt: Cole (Again)\nSFX: JSFXR.com\nMusic: TBD\n\n"+
+            "Programming: Cole Falxa-Sturken\nArt: Cole (Again)\nSFX: JSFXR.com\nMusic: F-777 'Space Battle'\n\n"+
             "Additional Credit to SnowBillr\n(SnowBillr's Blog) for button state code",
             {fontFamily: "Monospace", fontSize: "2vw"}
         ).setOrigin(0.4);
